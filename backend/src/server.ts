b17be3e73eb54b80ceaa012e1a8b653fde59445e
SamBonellGit <88200423+SamBonellGit@users.noreply.github.com>
2023-07-05 19:05:41 +0300

    Setup MongoDB connection, seperated server into server and app.ts for testing and clarity purposes, installed depencies

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,20 @@
-import express from "express";
-const app = express();
-const port = 5000;
+import app from "./app";
+import env from "./util/validateEnv";
+import mongoose from "mongoose";
 
 
-// sets a root to "/" and responds with "hello world" when called 
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+// sets a root to "/" and responds with "hello world" when called
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
- 
+
+
+const port = env.PORT || 5000;
+
+
+mongoose.connect(env.MONGO_CONNECTION_STRING).then(() => {
+  console.log("Connected to MongoDB");
+  app.listen(port, () => {
+    console.log(`Sam's App running on http://localhost:${port}`);
+  });
+})
+.catch(console.error);
